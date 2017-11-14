d4ca06b13d0228eb8120d793331f9f6701abf3bd
zhoufn <zhou_f_n@163.com>
2017-11-14 11:12:46 +0800

    新增获取执行任务和设置执行任务方法

--- ps-core/src/main/java/org/ps/platform/core/zookeeper/ZookeeperHandler.java
+++ ps-core/src/main/java/org/ps/platform/core/zookeeper/ZookeeperHandler.java
@@ -1,10 +1,15 @@
 package org.ps.platform.core.zookeeper;
 
+import com.alibaba.fastjson.JSON;
 import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
 import org.apache.zookeeper.data.Stat;
 import org.ps.platform.core.Constant;
+import org.ps.platform.core.Task;
+import org.ps.platform.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * zookeeper操作类
@@ -15,14 +20,58 @@ public class ZookeeperHandler {
     @Autowired
     private ZookeeperRegistryCenter registryCenter;
 
+    private String runningPath = "/" + Constant.NODE_TASK + "/" + Constant.NODE_TASK_RUNNING;
+
+    private String waitingPath = "/" + Constant.NODE_TASK + "/" + Constant.NODE_TASK_WAITING;
+
     /**
      * 初始化Task节点
      */
     public void initTaskTree() {
-        String runningPath = "/" + Constant.NODE_TASK + "/" + Constant.NODE_TASK_RUNNING;
-        String waitingPath = "/" + Constant.NODE_TASK + "/" + Constant.NODE_TASK_WAITING;
         this.createIfNotExist(runningPath);
         this.createIfNotExist(waitingPath);
+    }
+
+    /**
+     * 从TASK节点下获取执行的任务
+     *
+     * @return
+     */
+    public Task getRunningTask() {
+        try {
+            List<String> nodes = this.registryCenter.getClient().getChildren().forPath(runningPath);
+            if (nodes != null && nodes.size() > 0) {
+                byte[] bytes = this.registryCenter.getClient().getData().forPath(runningPath + "/" + nodes.get(0));
+                String string = new String(bytes);
+                return JSON.parseObject(string,Task.class);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+        return null;
+    }
+
+    /**
+     * 从等待的任务列表中获取任务
+     */
+    public void setRunningTask() {
+        try {
+            //判断当前有没有执行中的任务
+            List<String> children = this.registryCenter.getClient().getChildren().forPath(runningPath);
+            if (children == null || children.size() == 0) {
+                Task task = new Task();
+                task.setId(StringUtils.createUUID());
+                task.setCreateTime(System.currentTimeMillis());
+                task.setExecutor("demo");
+                task.setMonitor("demo");
+                task.setScheduler("demo");
+                task.setSort(1);
+                this.registryCenter.getClient().create().forPath(runningPath + "/" + task.getId(), JSON.toJSONString(task).getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
