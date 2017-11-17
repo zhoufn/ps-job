6559f3072307f1ee47ff1c36dcc7e1b45a8a002e
zhoufn <zhou_f_n@163.com>
2017-11-17 09:40:58 +0800

    初始化psadmin项目

---
+++ ps-admin/src/main/java/org/ps/PsAdminApp.java
@@ -0,0 +1,16 @@
+package org.ps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class PsAdminApp
+{
+    public static void main( String[] args ){
+        SpringApplication.run(PsAdminApp.class,args);
+    }
+}
