4477b028ed49a2d4f14bbaa3751b427f54acaa80
zhoufn <zhou_f_n@163.com>
2017-11-21 10:05:02 +0800

    引入异常机制，新增进度监控、生成Task报告。

--- ps-core/src/main/java/org/ps/example/demo01/Demo01MonitorHandler.java
+++ ps-core/src/main/java/org/ps/example/demo01/Demo01MonitorHandler.java
@@ -2,24 +2,47 @@ package org.ps.example.demo01;
 
 import org.ps.platform.core.Task;
 import org.ps.platform.core.annotation.IMonitor;
+import org.ps.platform.core.exception.MonitorException;
+import org.ps.platform.core.exception.ProcessException;
+import org.ps.platform.core.exception.ReportException;
 import org.ps.platform.core.handler.MonitorHandler;
 import org.springframework.stereotype.Component;
 
 @IMonitor(name = "demoMonitor")
 @Component
 public class Demo01MonitorHandler extends MonitorHandler {
+
     /**
      * Monitor周期性调用
      *
-     * @param task
+     * @param runningTask
      * @return Task是否已完成，true：完成，false：未完成。
      */
     @Override
-    public boolean isDown(Task task) {
-        System.out.println("******************");
-        if((System.currentTimeMillis() - task.getCreateTime() > 100000)){
-            return true;
-        }
+    public boolean isDown(Task runningTask) throws MonitorException {
+        System.out.println("*********** isDown of monitor for : " + runningTask.getId() + " *************");
         return false;
+    }
+
+    /**
+     * Monitor周期调用生成进度
+     *
+     * @param runningTask
+     * @return
+     */
+    @Override
+    public int process(Task runningTask) throws ProcessException {
+        System.out.println("*********** process of monitor for : " + runningTask.getId() + " *************");
+        return 0;
+    }
+
+    /**
+     * 生成任务报告
+     *
+     * @param runningTask
+     */
+    @Override
+    public void createReport(Task runningTask) throws ReportException {
+        System.out.println("*********** createReport of monitor for : " + runningTask.getId() + " *************");
     }
 }
