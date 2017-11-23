e559314c481ac1f0e978f04ecd3108996fbd3ecd
zhoufn <zhou_f_n@163.com>
2017-11-23 17:09:12 +0800

    完善demo03

--- ps-core/src/main/java/org/ps/example/demo03/AnalysisMonitor.java
+++ ps-core/src/main/java/org/ps/example/demo03/AnalysisMonitor.java
@@ -1,15 +1,24 @@
 package org.ps.example.demo03;
 
+import org.ps.example.demo02.UnPackageMonitor;
 import org.ps.platform.core.Task;
 import org.ps.platform.core.annotation.IMonitor;
 import org.ps.platform.core.exception.MonitorException;
 import org.ps.platform.core.exception.ReportException;
 import org.ps.platform.core.handler.MonitorHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @IMonitor(name = "analysisMonitor")
 public class AnalysisMonitor extends MonitorHandler{
+
+    /**
+     * 使用demo02的monitor
+     */
+    @Autowired
+    private UnPackageMonitor monitor;
+
     /**
      * 获取ShardTask总数
      *
@@ -18,7 +27,7 @@ public class AnalysisMonitor extends MonitorHandler{
      */
     @Override
     public int getTotalShardTaskCount(Task runningTask) throws MonitorException {
-        return 0;
+       return monitor.getTotalShardTaskCount(runningTask);
     }
 
     /**
@@ -29,7 +38,7 @@ public class AnalysisMonitor extends MonitorHandler{
      */
     @Override
     public int getDownShardTaskCount(Task runningTask) throws MonitorException {
-        return 0;
+        return monitor.getDownShardTaskCount(runningTask);
     }
 
     /**
