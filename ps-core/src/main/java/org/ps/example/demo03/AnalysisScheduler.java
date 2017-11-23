6d492984a1ea2878cefb44285d8f2d1ac015e577
zhoufn <zhou_f_n@163.com>
2017-11-23 17:13:52 +0800

    新增Demo03

--- ps-core/src/main/java/org/ps/example/demo03/AnalysisScheduler.java
+++ ps-core/src/main/java/org/ps/example/demo03/AnalysisScheduler.java
@@ -3,16 +3,12 @@ package org.ps.example.demo03;
 import com.alibaba.fastjson.JSON;
 import org.ps.example.demo03.domain.Param;
 import org.ps.example.demo03.handler.ScanHandler;
-import org.ps.platform.config.Configuration;
 import org.ps.platform.core.Task;
 import org.ps.platform.core.annotation.IScheduler;
 import org.ps.platform.core.exception.SchedulerException;
 import org.ps.platform.core.handler.SchedulerHandler;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
 
 @Component
 @IScheduler(name = "analysisScheduler")
